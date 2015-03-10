2507ec7d2f5d0ffa90b0203f8797cee6a4f529a3
Bence Eros <bence.eros@everit.biz>
2015-03-10 09:02:56 +0100

    making langfile configurable

--- src/main/java/logika/Main.java
+++ src/main/java/logika/Main.java
@@ -1,15 +1,32 @@
 package logika;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import logika.parser.Parser;
 import logika.parser.RecognitionException;
 
 public class Main {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws FileNotFoundException {
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar logika.jar <input-text> [<path-to-langfile>]");
+            return;
+        }
         String input = args[0];
+        InputStream langStream;
+        if (args.length >= 2) {
+            String langFilePath = args[1];
+            File langFile = new File(langFilePath);
+            langStream = new FileInputStream(langFile);
+        } else {
+            langStream = Main.class.getResourceAsStream("/lang1.xml");
+        }
         String output;
         try {
-            output = Parser.forString(input, Main.class.getResourceAsStream("/lang1.xml")).recognize();
+            output = Parser.forString(input, langStream).recognize();
         } catch (RecognitionException e) {
             output = e.getMessage();
         }
