ce295b090f5b2509e633aa7677fd57ff45d8d086
Bence Eros <ebence88@gmail.com>
2015-03-11 20:48:00 +0100

    ParserTest cleanup

--- src/test/java/logika/parser/ParserTest.java
+++ src/test/java/logika/parser/ParserTest.java
@@ -1,21 +1,20 @@
 package logika.parser;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ParserTest {
 
     @Test
     public void andOrImplWorks() {
-        Assert.assertEquals("", subject("and(P1(x, y), P2(u, v))").recognize());
-        Assert.assertEquals("", subject("or(P1(x, y), P2(u, v))").recognize());
-        Assert.assertEquals("", subject("impl(P1(x, y), P2(u, v))").recognize());
+        subject("and(P1(x, y), P2(u, v))").recognize();
+        subject("or(P1(x, y), P2(u, v))").recognize();
+        subject("impl(P1(x, y), P2(u, v))").recognize();
     }
 
     @Test
     public void notOperatorWorks() {
-        Assert.assertEquals("", subject("not(P1(x, y))").recognize());
+        subject("not(P1(x, y))").recognize();
     }
 
     @Test
@@ -35,7 +34,7 @@ public class ParserTest {
 
     @Test
     public void quantifiersWork() {
-        Assert.assertEquals("", subject("all(x, P1(x, y))").recognize());
+        subject("all(x, P1(x, y))").recognize();
     }
 
     private void recognitionException(final String input, final String expectedMessage) {
@@ -45,12 +44,6 @@ public class ParserTest {
         } catch (RecognitionException e) {
             Assert.assertEquals(expectedMessage, e.getMessage());
         }
-    }
-
-    @Test
-    @Ignore
-    public void singleVariableResult() {
-        Assert.assertEquals("x is a free variable", subject("x").recognize());
     }
 
     private Parser subject(final String input) {
