59301338783bba1d5492774f1fe0336b8a05e6fb
Bence Eros <ebence88@gmail.com>
2015-03-25 00:14:22 +0100

    NodeVisitorBase initial impl

---
+++ src/main/java/logika/model/ast/visitor/NodeVisitorBase.java
@@ -0,0 +1,65 @@
+package logika.model.ast.visitor;
+
+import logika.model.ast.BinaryOpNode;
+import logika.model.ast.ConstantNode;
+import logika.model.ast.FunctionNode;
+import logika.model.ast.Node;
+import logika.model.ast.PredicateNode;
+import logika.model.ast.QuantifierNode;
+import logika.model.ast.UnaryOpNode;
+import logika.model.ast.VarNode;
+
+public class NodeVisitorBase<R> implements NodeVisitor<R> {
+
+    @Override
+    public R accumulate(final R previous, final R current) {
+        return current;
+    }
+
+    @Override
+    public R identity() {
+        return null;
+    }
+
+    @Override
+    public R visitBinaryOperator(final BinaryOpNode node) {
+        return visitChildren(node);
+    }
+
+    private R visitChildren(final Node node) {
+        return node.getChildren().stream()
+                .map((n) -> n.accept(this))
+                .reduce(identity(), this::accumulate);
+    }
+
+    @Override
+    public R visitConstant(final ConstantNode node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public R visitFunction(final FunctionNode node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public R visitPredicate(final PredicateNode node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public R visitQuantifier(final QuantifierNode node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public R visitUnaryOperator(final UnaryOpNode node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public R visitVar(final VarNode node) {
+        return visitChildren(node);
+    }
+
+}
